40dc481e79b0121986ebbca3acb10f7eb72bd1bf
Antoine Bernier <31347745+Bernier154@users.noreply.github.com>
2025-10-19 00:32:56 -0400

    correction preventing icon

--- scenes/window_download_combiner.gd
+++ scenes/window_download_combiner.gd
@@ -4,6 +4,19 @@ extends WindowIndexed
 @onready var Download2: = $PanelContainer / MainContainer / Download2
 @onready var download_out: = $PanelContainer / MainContainer / DownloadOut
 
+const MOD_DIR := "bernier154-network_combiner"
+var mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(MOD_DIR)
+
 
 func process(delta: float) -> void :
 	download_out.count = Download.count + Download2.count
+
+func export() -> Dictionary:
+	return super().merged({
+		"filename": "../../"+ mod_dir_path.replace('res://','') +"/scenes/"+scene_file_path.get_file(), 
+	},true)
+
+func save() -> Dictionary:
+	return super().merged({
+		"filename": "../../"+ mod_dir_path.replace('res://','') +"/scenes/"+scene_file_path.get_file(), 
+	},true)
